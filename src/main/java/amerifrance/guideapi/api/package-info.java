84e7a0e84762c758529549c797a5fb88615b6051
TehNut <nick_ignoffo@yahoo.com>
2015-06-01 20:29:29 -0700

    Update version to 1.0.1

--- src/main/java/amerifrance/guideapi/api/package-info.java
+++ src/main/java/amerifrance/guideapi/api/package-info.java
@@ -1,3 +1,4 @@
-@API(owner = "guideapi", apiVersion = "@VERSION@", provides = "Guide-API|API") package amerifrance.guideapi.api;
+@API(owner = "guideapi", apiVersion = "@VERSION@", provides = "Guide-API|API")
+package amerifrance.guideapi.api;
 
 import cpw.mods.fml.common.API;
