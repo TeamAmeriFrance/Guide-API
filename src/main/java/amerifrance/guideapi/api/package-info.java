6d2f70637a6ee1eed3315daa32714aa011861748
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-05-05 18:46:51 -0700

    Fix code version replacements + update version

--- src/main/java/amerifrance/guideapi/api/package-info.java
+++ src/main/java/amerifrance/guideapi/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "guideapi", apiVersion = "@VERSION@", provides = "Guide-API|API")
+@API(owner = "guideapi", apiVersion = "@API_VERSION@", provides = "Guide-API|API")
 package amerifrance.guideapi.api;
 
 import net.minecraftforge.fml.common.API;
