c034cf0ef5f4cefd8e7d862089858b255834e7f4
maxanier <dev@maxgb.de>
2021-02-13 11:49:40 +0100

    Fix opening external web link

--- src/main/java/de/maxanier/guideapi/page/PageHolderWithLinks.java
+++ src/main/java/de/maxanier/guideapi/page/PageHolderWithLinks.java
@@ -21,7 +21,6 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.ForgeHooks;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -179,16 +178,7 @@ public class PageHolderWithLinks implements IPage {
 
         @Override
         public void onClicked(Book book, CategoryAbstract category, EntryAbstract entry, PlayerEntity player, ItemStack bookStack, int page) {
-            try {
-                Class<?> oclass = Class.forName("java.awt.Desktop");
-                Object object = oclass.getMethod("getDesktop", new Class[0]).invoke(null);
-                oclass.getMethod("browse", new Class[]{URI.class}).invoke(object, link);
-            } catch (Throwable throwable1) {
-                Throwable throwable = throwable1.getCause();
-                LOGGER.error("Couldn't open link: {}", link);
-                LOGGER.catching(throwable);
-                player.sendMessage(ForgeHooks.newChatWithLinks("Couldn't open link: " + link.toString()), Util.DUMMY_UUID);
-            }
+            Util.getOSType().openURI(link);
         }
     }
 
